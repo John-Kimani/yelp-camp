31bf019ae5dbdb8284e61970220e50847272fbd9
John-Kimani <kimwaweru100@gmail.com>
2022-08-28 00:41:26 +0300

    Route: Login and home pages

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/Home/Auth/Login'
+import HomePage from './pages/Home/Home'
 
 export interface IApplicationProps {};
 
 const Application = () => {
   return (
-    <div>Application</div>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
