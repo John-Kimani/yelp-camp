cdb983a97c8d33063a76319a9ba39c18598f59a7
John-Kimani <kimwaweru100@gmail.com>
2022-08-28 01:48:07 +0300

    Signout: Enable signout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { getAuth, signOut } from 'firebase/auth';
 
 export interface IHomePageProps {};
 
 const HomePage: React.FC<IHomePageProps> = (props) => {
+
+  const auth = getAuth()
   return (
     <div>
-        <p>Home page</p>
+        <p>Home page (protected with firebase)</p>
+        <button onClick={()=> signOut(auth)}>Sign Out of Firebase</button>
     </div>
   )
 };
