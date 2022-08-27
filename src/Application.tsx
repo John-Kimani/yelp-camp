9d2f99dbf8a8595bcb1704bce89cfc97da78bf60
John-Kimani <kimwaweru100@gmail.com>
2022-08-28 01:03:00 +0300

    Firebase: Install and initialize application

--- src/Application.tsx
+++ src/Application.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/Home/Auth/Login'
 import HomePage from './pages/Home/Home'
+import { initializeApp } from 'firebase/app'
+import { config } from './config/config'
+
+initializeApp(config.firebaseConfig);
 
 export interface IApplicationProps {};
 
