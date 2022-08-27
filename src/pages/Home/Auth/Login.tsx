facb7910dbe8d10e7fcaf3d7624c13a56b1e3cdb
John-Kimani <kimwaweru100@gmail.com>
2022-08-28 01:42:16 +0300

    Login: enable login with google

--- src/pages/Home/Auth/Login.tsx
+++ src/pages/Home/Auth/Login.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 export interface ILoginPageProps {};
 
 const LoginPage: React.FC<ILoginPageProps> = (props) => {
+
+  const auth = getAuth();
+
+  const navigate = useNavigate();
+
+  const [authing, setAuthing] = useState(false);
+
+  const signInWithGoogle =async () => {
+    setAuthing(true);
+
+    signInWithPopup(auth, new GoogleAuthProvider()).then(
+      response => {
+        console.log(response.user.uid);
+        navigate('/')
+      }
+    ).catch(
+      error => {
+        console.log(error);
+        setAuthing(false);
+      }
+    )
+  };
+
   return (
     <div>
         <p>This is the login page</p>
-        <button>Sign In with Google</button>
+        <button onClick={()=> signInWithGoogle()} disabled={authing}>Sign In with Google</button>
     </div>
   )
 };
