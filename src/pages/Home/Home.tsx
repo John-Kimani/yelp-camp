d71505c358e6255b57c4578e5a20158691bf9db2
John-Kimani <kimwaweru100@gmail.com>
2022-08-28 19:31:40 +0300

    Clean up source files and code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { getAuth, signOut } from 'firebase/auth';
-import PlacesComponent, {IPlacesComponent} from '../../components/Places';
+import PlacesComponent from '../../components/Places';
 import placesData from './data/places.json';
 
 export interface IHomePageProps {};
@@ -8,25 +8,6 @@ export interface IHomePageProps {};
 const HomePage: React.FC<IHomePageProps> = (props) => {
 
   const auth = getAuth()
-
-  // const [listOfPlaces , setListOfPlaces] = useState([])
-
-  // const url = "./data/json/data.JSON"
-
-  // useEffect(() => {
-
-  //   fetch(url).then(
-  //     response => {
-  //       console.log(response.json())
-  //       return response.json()
-  //     }
-  //   ).then(
-  //     (data: any) => {
-  //       setListOfPlaces(data)
-  //     }
-  //   )
-    
-  // }, [])
 
   const placeList = placesData.map((place:any, index:number) => {
     return <PlacesComponent key={index} name={place.name} imageUrl={place.imageUrl} description={place.description}/>
